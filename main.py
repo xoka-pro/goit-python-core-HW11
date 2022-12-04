6bd00db8fd512df68ee884b8fd5a98f6b44335bc
Oleh Ovchinnikov <xokzzz@gmail.com>
2022-12-04 15:12:21 +0200

    finish

--- main.py
+++ main.py
@@ -1,54 +1,97 @@
 from collections import UserDict
+import datetime
+import re
 
 
 class AddressBook(UserDict):
-    """
-    Клас AddressBook, який успадковується від UserDict,
-    та ми потім додамо логіку пошуку за записами до цього класу.
-
-    * AddressBook реалізує метод add_record, який додає Record у self.data."""
+    """Клас AddressBook - зберігає, додає записи та віддає записи книги контактів через ітератор"""
 
     def add_record(self, record):
         self.data[record.name.value] = record
 
+    def iterator(self, count: int):
+        for key, value in self:
+            i = 1
+            container = {}
+            while i <= count:
+                container[key] = value
+                i += 1
+            yield container
+
+    def __iter__(self):
+        for key, value in self.data.items():
+            yield key, value
+
 
 class Field:
-    """Клас Field, який буде батьківським для всіх полів,
-    у ньому потім реалізуємо логіку загальну для всіх полів"""
-    pass
+    """Батьківський клас для всіх записів у книзі контактів"""
+    def __init__(self, value=None):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
 
 
 class Name(Field):
-    """Клас Name, обов'язкове поле з ім'ям."""
+    """Обов'язкове поле з ім'ям в книзі контактів"""
 
     def __init__(self, value):
+        super().__init__(value)
         self.value = value
 
 
 class Phone(Field):
-    """Клас Phone, необов'язкове поле з телефоном
-    та таких один запис (Record) може містити кілька."""
+    """Необов'язкове поле з номером телефону(або кількома)"""
 
     def __init__(self, phone):
-        self.value = phone
+        super().__init__()
+        self.value = Phone.check_phone(phone)
+
+    @classmethod
+    def check_phone(cls, phone):
+        """Метод для валідації синтаксису номера телефона"""
+        regex = r"\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}"
+        if len(re.findall(regex, phone)) == 0:
+            raise ValueError('Incorrect phone format, should be (XXX)AAA-BB-CC')
+        return phone
+
+    @Field.value.setter
+    def value(self, value):
+        super(Phone, self.__class__).value.fset(self, Phone.check_phone(value))
+
+
+class Birthday(Field):
+    """Необов'язкове поле з датою народження"""
+
+    @Field.value.setter
+    def value(self, value):
+        try:
+            super(Birthday, self.__class__).value.fset(self, datetime.datetime.strptime(value, '%d-%m-%Y'))
+        except ValueError:
+            print('Incorrect date format, should be DD-MM-YYYY')
 
 
 class Record(Field):
-    """Клас Record, який відповідає за логіку додавання/видалення/редагування
-    необов'язкових полів та зберігання обов'язкового поля Name.
+    """Відповідає за логіку додавання/видалення/редагування полів.
+    А також реалізує метод розрахунку днів до наступного дня народження(якщо параметр задано для поля)"""
 
-    * Записи Record у AddressBook зберігаються як значення у словнику.
-    В якості ключів використовується значення Record.name.value.
-    * Record зберігає об'єкт Name в окремому атрибуті.
-    * Record зберігає список об'єктів Phone в окремому атрибуті.
-    * Record реалізує методи для додавання/видалення/редагування об'єктів Phone."""
-
-    def __init__(self, name, phone=None):
+    def __init__(self, name, phone=None, birthday=None):
+        super().__init__()
         self.name = Name(name)
+        self.birthday = Birthday()
+
         if phone:
             self.phones = [Phone(phone)]
         else:
             self.phones = []
+
+        if birthday:
+            self.birthday.value = birthday
 
     def add_phone(self, phone):
         self.phones.append(Phone(phone))
@@ -59,9 +102,24 @@ class Record(Field):
                 el.value = new_phone
 
     def del_phone(self, phone):
-        for elem in self.phones:
-            if elem.value == phone:
-                self.phones.remove(elem)
+        for el in self.phones:
+            if el.value == phone:
+                self.phones.remove(el)
+
+    def days_to_birthday(self):
+        today = datetime.date.today()
+        birthday = self.birthday.value
+        next_birthday = datetime.date(year=today.year, month=birthday.month, day=birthday.day)
+
+        if next_birthday < today:
+            next_birthday = datetime.date(year=today.year + 1, month=birthday.month, day=birthday.day)
+
+        delta = next_birthday - today
+
+        if delta.days == 0:
+            return "birthday today!"
+        else:
+            return f'{delta.days} days to birthday'
 
 
 contacts = AddressBook()
@@ -77,31 +135,30 @@ def input_error(func):
         except IndexError as error:
             return f'Sorry, not enough params for command. Error: {error}'
         except ValueError as error:
-            return f'Give me name and phone, please. Error: {error}'
+            return f'Value error: {error}'
         except TypeError as error:
             return f'Not enough arguments. Error: {error}'
     return wrapper
 
 
 def hello() -> str:
-    return f'How can I help you?'
+    """Функція для вітання користувача"""
+    return (f'How can I help you?\n'
+            f'Type "h" or "help" to show help')
 
 
 def goodbye():
+    """Функція завершення програми"""
     print(f'Good bye!')
     quit()
 
 
 @input_error
-def add(*args) -> str:
-    """
-    "add ...". За цією командою бот зберігає у пам'яті (у словнику, наприклад) новий контакт.
-    Замість ... користувач вводить ім'я та номер телефону, обов'язково через пробіл.
-    """
+def add(name, number, birthday=None) -> str:
+    """Функція для додавання нового запису або додавання нового телефону контакту"""
 
-    name, number, *_ = args
-    if not name in contacts:
-        new_number = Record(name, number)
+    if name not in contacts:
+        new_number = Record(name, number, birthday)
         contacts.add_record(new_number)
         return f'Contact add successfully'
     else:
@@ -111,10 +168,7 @@ def add(*args) -> str:
 
 @input_error
 def change(*args) -> str:
-    """
-    "change ..." За цією командою бот зберігає в пам'яті новий номер телефону існуючого контакту.
-    Замість ... користувач вводить ім'я та номер телефону, обов'язково через пробіл.
-    """
+    """Функція для заміни номеру телефона контакту"""
 
     name, old_number, new_number, *_ = args
     if name in contacts:
@@ -126,6 +180,7 @@ def change(*args) -> str:
 
 @input_error
 def del_phone(name, phone) -> str:
+    """Функція видалення номера телефона у контакту"""
 
     if name in contacts:
         contacts[name].del_phone(phone)
@@ -136,10 +191,7 @@ def del_phone(name, phone) -> str:
 
 @input_error
 def phone_func(*args) -> str:
-    """
-    "phone ...." За цією командою бот виводить у консоль номер телефону для зазначеного контакту.
-    Замість ... користувач вводить ім'я контакту, чий номер треба показати.
-    """
+    """Повертає номер телефону для зазначеного контакту"""
 
     name = args[0]
     if name in contacts:
@@ -151,20 +203,33 @@ def phone_func(*args) -> str:
 
 @input_error
 def show_all() -> str:
-    """
-    "show all". За цією командою бот виводить всі збереженні контакти з номерами телефонів у консоль.
-    """
+    """Повертає всю книгу контактів"""
 
     result = []
-    for name, numbers in contacts.items():
-        result.append(f'Name: {name} | Numbers: {", ".join(phone.value for phone in numbers.phones)}')
+    for el in contacts.iterator(5):
+        for name, data in el.items():
+            numbers = ", ".join(phone.value for phone in data.phones)
+            if data.birthday.value:
+                bday = data.birthday.value.date().strftime('%d-%m-%Y')
+                to_birthday = contacts[name].days_to_birthday()
+                result.append(f'Name: {name} | Numbers: {numbers} | Birthday: {bday} - {to_birthday}')
+            else:
+                result.append(f'Name: {name} | Numbers: {numbers}')
     if len(result) < 1:
         return f'Contact list is empty'
     return '\n'.join(result)
 
 
 def hlp(*args) -> str:
-    return f'Known commands: hello, help, add, change, phone, show all, delete, good bye, close, exit.'
+    """Повертає коротку допомогу по командах"""
+    return (f'Known commands:\n'
+            f'hello, help -- this help\n'
+            f'add -- add new contact or new number for contact\n'
+            f'change -- change specified number for contact\n'
+            f'phone --  show phone numbers for specified contact\n'
+            f'show all -- show all contacts\n'
+            f'delete -- delete specified number from contact\n'
+            f'good bye, close, exit -- shutdown application')
 
 
 def parser(msg: str):
