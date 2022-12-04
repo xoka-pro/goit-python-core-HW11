4ed458697805817272f49d47115b3d4de7d8b807
Oleh Ovchinnikov <xokzzz@gmail.com>
2022-12-04 17:21:17 +0200

    make corrections

--- main.py
+++ main.py
@@ -1,126 +1,5 @@
-from collections import UserDict
-import datetime
-import re
-
-
-class AddressBook(UserDict):
-    """Клас AddressBook - зберігає, додає записи та віддає записи книги контактів через ітератор"""
-
-    def add_record(self, record):
-        self.data[record.name.value] = record
-
-    def iterator(self, count: int):
-        for key, value in self:
-            i = 1
-            container = {}
-            while i <= count:
-                container[key] = value
-                i += 1
-            yield container
-
-    def __iter__(self):
-        for key, value in self.data.items():
-            yield key, value
-
-
-class Field:
-    """Батьківський клас для всіх записів у книзі контактів"""
-    def __init__(self, value=None):
-        self._value = value
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, value):
-        self._value = value
-
-
-class Name(Field):
-    """Обов'язкове поле з ім'ям в книзі контактів"""
-
-    def __init__(self, value):
-        super().__init__(value)
-        self.value = value
-
-
-class Phone(Field):
-    """Необов'язкове поле з номером телефону(або кількома)"""
-
-    def __init__(self, phone):
-        super().__init__()
-        self.value = Phone.check_phone(phone)
-
-    @classmethod
-    def check_phone(cls, phone):
-        """Метод для валідації синтаксису номера телефона"""
-        regex = r"\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}"
-        if len(re.findall(regex, phone)) == 0:
-            raise ValueError('Incorrect phone format, should be (XXX)AAA-BB-CC')
-        return phone
-
-    @Field.value.setter
-    def value(self, value):
-        super(Phone, self.__class__).value.fset(self, Phone.check_phone(value))
-
-
-class Birthday(Field):
-    """Необов'язкове поле з датою народження"""
-
-    @Field.value.setter
-    def value(self, value):
-        try:
-            super(Birthday, self.__class__).value.fset(self, datetime.datetime.strptime(value, '%d-%m-%Y'))
-        except ValueError:
-            print('Incorrect date format, should be DD-MM-YYYY')
-
-
-class Record(Field):
-    """Відповідає за логіку додавання/видалення/редагування полів.
-    А також реалізує метод розрахунку днів до наступного дня народження(якщо параметр задано для поля)"""
-
-    def __init__(self, name, phone=None, birthday=None):
-        super().__init__()
-        self.name = Name(name)
-        self.birthday = Birthday()
-
-        if phone:
-            self.phones = [Phone(phone)]
-        else:
-            self.phones = []
-
-        if birthday:
-            self.birthday.value = birthday
-
-    def add_phone(self, phone):
-        self.phones.append(Phone(phone))
-
-    def change_phone(self, old_phone, new_phone):
-        for el in self.phones:
-            if el.value == old_phone:
-                el.value = new_phone
-
-    def del_phone(self, phone):
-        for el in self.phones:
-            if el.value == phone:
-                self.phones.remove(el)
-
-    def days_to_birthday(self):
-        today = datetime.date.today()
-        birthday = self.birthday.value
-        next_birthday = datetime.date(year=today.year, month=birthday.month, day=birthday.day)
-
-        if next_birthday < today:
-            next_birthday = datetime.date(year=today.year + 1, month=birthday.month, day=birthday.day)
-
-        delta = next_birthday - today
-
-        if delta.days == 0:
-            return "birthday today!"
-        else:
-            return f'{delta.days} days to birthday'
-
+from classes import AddressBook
+from classes import Record
 
 contacts = AddressBook()
 
@@ -189,16 +68,18 @@ def del_phone(name, phone) -> str:
     return f'Phone number deleted successfully'
 
 
-@input_error
+# @input_error
 def phone_func(*args) -> str:
     """Повертає номер телефону для зазначеного контакту"""
 
     name = args[0]
-    if name in contacts:
-        for name, numbers in contacts.items():
-            return f'Name: {name} | Numbers: {", ".join(phone.value for phone in numbers.phones)}'
-    else:
-        return f'No contact "{name}"'
+
+    for el in contacts.iterator(5):
+        for key, data in el.items():
+            if key == name:
+                return f'Name: {key} | Numbers: {", ".join(phone.value for phone in data.phones)}'
+            else:
+                return f'No contact "{name}"'
 
 
 @input_error
